ff10174b3398b165e5ee5cf3a1e9238acff34772
hayathasan <hayat.hasan@gmail.com>
2020-12-25 23:54:26 +0600

    ''

--- 4.boolean.php
+++ 4.boolean.php
@@ -1,13 +1,13 @@
 <?php
 
     $a = 2;
-    echo "a = a : $a\n";
+    echo "a = 2 : $a\n";
     
-    $b = $a;
-    echo "b = a : $b<br>";
+    $b = &$a;
+    echo "b = &a : $b\n";
     
     $a += 1;
-    echo "a += 1 : $a<br>";
-    echo "b = b : $b<br>";
+    echo "a += 1 : $a\n";
+    echo "b = b : $b\n";
 
 ?>
