c9747e4fcbade85017a778b7d6f95f97065f14f8
Tamim Ibrahim <tamimibrahim17@gmail.com>
2020-12-28 12:43:06 +0600

    [learning path]

--- integers.php
+++ integers.php
@@ -1,2 +1,17 @@
 // Learn about integers in PHP
 // https://www.php.net/manual/en/language.types.integer.php
+
+// Learn how to convert string to integer
+// https://www.php.net/manual/en/function.intval.php
+
+// Learn why this is falsey -> ( '1' === 1 )
+
+// Learn how do you know if variable is numeric or not?
+// $a = true;
+// $b = '0';
+// $c = 'hello';
+function isNumeric($var)
+{
+  // return true if $var is numeric else false
+}
+
