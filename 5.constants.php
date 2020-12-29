73924ac3080a602ea87e1c4d738b4ac8afacd3e6
hayathasan <hayat.hasan@gmail.com>
2020-12-29 23:27:26 +0600

    ''

--- 5.constants.php
+++ 5.constants.php
@@ -1,4 +1,14 @@
 <?php
-define("foo","this is foo constant");
+//normal constant
+define("foo","This is normal constant\n\n");
 echo foo;
+
+
+//class constant
+class classConstant {
+    const FOO_CLASS = "This is class constant\n\n";    
+}
+
+$class_constant = classConstant::FOO_CLASS;
+echo $class_constant;
 ?>
