f04fe9d7a4963b63f57bdc18a5650b6f305ccc01
tamimibrahim17 <tamimibrahim17@gmail.com>
2020-12-30 13:27:27 +0600

    [code improvement] constant style fix

--- 5.constants.php
+++ 5.constants.php
@@ -1,15 +1,29 @@
 <?php
 //normal constant
-define("foo","This is normal constant\n\n");
-echo foo;
+// define("foo","This is normal constant\n\n"); // This is bad style
+
+// Constant must be uppercase
+define('FOO', "This is normal constant.");
+echo FOO;
+
+// echo foo;
 
 
 //class constant
-class classConstant {
-    const FOO_CLASS = "This is class constant\n\n";    
+// class classConstant {
+//     const FOO_CLASS = "This is class constant\n\n";    
+// }
+
+// Class name should be CamelCase
+// style guide : https://gist.github.com/ryansechrest/8138375
+class ClassConstant {
+
+	const FOO_CLASS = "This is class constant\n\n";
+	
 }
 
-$class_constant = classConstant::FOO_CLASS;
+// $class_constant = classConstant::FOO_CLASS;
+$class_constant = ClassConstant::FOO_CLASS;
 echo $class_constant;
 
 ?>
