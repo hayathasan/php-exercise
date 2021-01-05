d31f080040c946d88347f524e00c6ef392cdd116
hayathasan <hayat.hasan@gmail.com>
2021-01-05 23:59:14 +0600

    learning in progress

--- 9.class.php
+++ 9.class.php
@@ -2,22 +2,23 @@
 print "\n\n--- static and non-static method ---\n\n";
 
 class Foo{
-    private $a = "a";
+    public $a = "a";
     public function aa(){
         $this->a = "aa\n";
-        echo $this->a;
+        return $this->a;
     }
 
     static $b = "b";
     public static function bb(){
         self::$b = "bb\n";
-        echo self::$b;
+        return self::$b;
     }
 }
 
 //non-static method called via class instantiation 
 $foo = new Foo();
-echo $foo->aa();
+echo "property: $foo->a \n";
+echo "menthod: {$foo->aa()}";
 
 //static method called with scope resolution operator
 echo Foo::bb();
